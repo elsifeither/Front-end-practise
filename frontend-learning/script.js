554dff2132a748b25238c86ca375e7757dc7c141
GRIGS <ivkata_491@abv.bg>
2026-01-09 17:03:37 +0200

    Add user age filter practice project

--- frontend-learning/script.js
+++ frontend-learning/script.js
@@ -1,6 +1,31 @@
-const add = (a,b) => a + b;
+const users = [
+    {name: "Ivan", age: 20},
+    {name:"Pesho", age: 23},
+    {name: "Kosta", age: 16},
+    {name: "Gosho", age: 14}
+];
 
-console.log(add(3,5));
+const userList = document.getElementById("userList");
+const showAllBtn = document.getElementById("showAll");
+const showAdultsBtn = document.getElementById("showAdults");
+
+const renderUsers = (usersToRender) => {
+    userList.innerHTML = "";
+
+    usersToRender.forEach(user => {
+        const li = document.createElement("li");
+        li.textContent = `${user.name} (${user.age})`;
+        userList.appendChild(li); 
+    });
+};
+
+showAllBtn.addEventListener("click", () => {
+    renderUsers(users);
+});
 
 
+showAdultsBtn.addEventListener("click", () => {
+    const adults = users.filter( user => user.age >= 18);
+    renderUsers(adults);
+});
 
